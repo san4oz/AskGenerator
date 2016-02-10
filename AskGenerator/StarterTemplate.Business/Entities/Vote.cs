ed246ec6c30e67e94c6d3b80b2534b567128cc0d
BilyachenkoOY <iviviv1901@gmail.com>
2016-02-11 00:13:32 +0200

    vote classes

---
+++ AskGenerator/StarterTemplate.Business/Entities/Vote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AskGenerator.Business.Entities
+{
+    public class Vote : Entity
+    {
+        public Question Question { get; set; }
+
+        public short Answer { get; set; }
+
+        public Teacher Teacher { get; set; }
+
+        public User Account { get; set; }
+    }
+}
