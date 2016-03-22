98412b5e749249157cef323324b9d6730769dbf1
BilyachenkoOY <iviviv1901@gmail.com>
2016-03-22 10:47:31 +0200

    info

---
+++ AskGenerator/Mvc/Controllers/InfoController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace AskGenerator.Mvc.Controllers
+{
+    public class InfoController : BaseController
+    {
+        /// <summary>
+        /// Index action to return site politics.
+        /// </summary>
+        /// <returns>ViewResult or PartialViewResult.</returns>
+        [OutputCache(CacheProfile = "Cache1Hour")]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            if (Request.IsAjaxRequest())
+                return PartialView();
+            return View();
+        }
+
+        /// <summary>
+        /// Index action to return site politics.
+        /// </summary>
+        /// <returns>ViewResult or PartialViewResult.</returns>
+        [OutputCache(CacheProfile = "Cache1Hour", Location = OutputCacheLocation.Any)]
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult IndexPost()
+        {
+            if (Request.IsAjaxRequest())
+                return PartialView();
+            return View();
+        }
+
+        /// <summary>
+        /// Action to return calculation rules.
+        /// </summary>
+        /// <returns>ViewResult or PartialViewResult.</returns>
+        [OutputCache(CacheProfile = "Cache1Hour")]
+        [HttpGet]
+        public ActionResult Calculation()
+        {
+            if (Request.IsAjaxRequest())
+                return PartialView();
+            return View();
+        }
+
+        /// <summary>
+        /// Action to return calculation rules.
+        /// </summary>
+        /// <returns>ViewResult or PartialViewResult.</returns>
+        [OutputCache(CacheProfile = "Cache1Hour", Location = OutputCacheLocation.Any)]
+        [HttpPost]
+        [ActionName("Calculation")]
+        public ActionResult CalculationPost()
+        {
+            if (Request.IsAjaxRequest())
+                return PartialView();
+            return View();
+        }
+    }
+}
---
+++ AskGenerator/StarterTemplate/Scripts/pages/info.js
@@ -0,0 +1,51 @@
+﻿"use strict";
+var Pages = Pages || {};
+Pages.Info = (function () {
+    var self = {};
+
+    var initTabs = function () {
+        var page = $('#infoPage');
+        var tabs = $('.infoPageWrapper .nav-tabs li');
+
+        var loadHtml = function (url, target) {
+            tabs.removeClass('active');
+            target.addClass('active');
+
+            page.empty();
+            page.addClass('loading');
+            $.post(url, null, function (data) {
+                page.removeClass('loading');
+                page.html(data);
+            });
+        };
+
+        tabs.click(function (e) {
+            var target = $(this);
+            var url = target.find('a').attr('href');
+            if (window.history.pushState) {
+                loadHtml(url, target);
+
+                var title = $('title').text();
+                window.history.pushState({ url: location.pathname }, title, url);
+            } else {
+                location.href = url;
+            }
+        });
+
+        window.onpopstate = function (event) {
+            if (event.state.url) {
+                var tab = tabs.find('a[href="' + event.state.url + '"').parent();
+                if (tab.length) {
+                    loadHtml(event.state.url, tab);
+                    return;
+                }
+            }
+            location.href = document.location;
+        };
+    };
+
+    self.init = function () {
+        initTabs();
+    };
+    return self;
+})();
