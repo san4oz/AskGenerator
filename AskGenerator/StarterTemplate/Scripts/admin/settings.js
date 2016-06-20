de87b472448c06f2eca1a660b406b31e8f17b83d
BIlyachenkoOY <iviviv1901@gmail.com>
2016-06-21 01:29:15 +0300

    datepickker for iterations

---
+++ AskGenerator/StarterTemplate/Scripts/admin/settings.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+var Pages = Pages || {};
+Pages.Settings = (function () {
+    var self = {};
+
+    self.initGeneral = function () {
+        $('.date').datetimepicker({
+            format: 'd/m/Y H:i'
+        });
+    };
+
+    return self;
+})();
